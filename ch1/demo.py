53c66750285b573f64db0f3540b5ce1bb84b5d26
Sean Hsu <sean.hsu@kkday.com>
2018-11-28 10:51:33 +0800

    init

---
+++ ch1/demo.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+print('Hello, world!')
+
+num1 = 1.5
+num2 = 6.3
+
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# 這是註解
+
+""" 這也是註解 """
+
+"""
+這是多行註解
+多
+行
+你
+懂
+的
+"""
+
+# python3 -m compileall .
+# python3 -O -m compileall .
