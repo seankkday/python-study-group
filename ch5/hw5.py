16c6f9696e736551953183fc8deb41b3975c8a34
Sean Hsu <sean.hsu@kkday.com>
2019-01-23 11:40:51 +0800

    feat: add hw5

---
+++ ch5/hw5.py
@@ -0,0 +1,19 @@
+"""
+1. 解釋 stack 跟 queue 是什麼? 他們的差別是?
+"""
+
+"""
+2. 實做一個queue class kqueue, 並提供 kqueue.push(), kqueue.pop(), kqueue.size(), kqueue.front(), kqueue.back() 的功能
+"""
+
+"""
+3. 實做一個stack class kstack, 並提供 kstack.push(), kstack.pop(), kstack.size(), kstack.top() 的功能
+"""
+
+"""
+4. 將上面兩個分別打包成 ktool 的 module
+"""
+
+"""
+5. 分別使用 ktool 裡的 kqueue 及 kstack 對 [2,3,1,9,22,31,8,7,45,9] 由小到大做排序
+"""
